af09c94a170345208be50fda76f152f1f90c57c3
VinodKumarReddy V <61618854+VinodKumarReddy-V@users.noreply.github.com>
2020-03-20 21:47:12 +0530

    Add files via upload
    First class code from Code create By U

---
+++ Hello.c
@@ -0,0 +1,25 @@
+// 1. Write a program to Print Hello World Using C Program
+#include<stdio.h> 
+void main()
+{
+    printf("HelloWorld");
+    printf("\n Welcome to Code Create By U");
+}
+// 2. Write a program to Print Your Name and Age
+#include"stdio.h"
+void main()
+{
+    printf("%s,%d","Nani",23);
+} 
+// 3. Write a program Sum of 3 numbers 
+#include<stdio.h>
+void main()
+{
+    int a,b,c,Total;
+    a =5;
+    b=10;
+    c=30;
+    Total = a + b +c ;
+    printf("Sum of 3 numbers = %d",Total);
+  
+}
